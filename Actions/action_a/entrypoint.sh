a083d4f94511df44c0dcfff9feaa19af81e000cb
RikardGard <rikard.gard@gmail.com>
2021-10-14 11:21:24 +0200

    my occu

--- Actions/action_a/entrypoint.sh
+++ Actions/action_a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAMES and I love my job which is $INPUT_MY_occu"
+sh -c "echo Hello world my name is $INPUT_MY_NAMES and I love my job which is $INPUT_MY_OCCU "
